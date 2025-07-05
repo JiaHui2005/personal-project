652b6eec776be2208cd5417882afaf6d4b4b5e83
JiaHui2005 <vogiahuypro9@gmail.com>
2025-07-05 15:40:14 +0700

    Fix Cart Layout

--- WebBanHangMVC/Controllers/CartController.cs
+++ WebBanHangMVC/Controllers/CartController.cs
@@ -8,7 +8,7 @@ public class CartController : Controller
     {
         var fakeCart = new List<CartItem>
         {
-            new CartItem { ProductId = 1, ProductName = "Sản phẩm 1", Quantity = 2, Price = 10000, ImageUrl = "\\Image\\test.jpg" },
+            new CartItem { ProductId = 1, ProductName = "PC Faster Gaming 10400F - RTX 3050 6GB (All New - Bộ hoàn)", Quantity = 2, Price = 10000, ImageUrl = "\\Image\\test.jpg" },
             new CartItem { ProductId = 2, ProductName = "Sản phẩm 2", Quantity = 1, Price = 20000, ImageUrl = "\\Image\\test.jpg" },
             new CartItem { ProductId = 3, ProductName = "Sản phẩm 3", Quantity = 3, Price = 15000, ImageUrl = "\\Image\\test.jpg" }
         };
